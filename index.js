b441f8fee2627854d4b41933cf7ca80d1c43788b
Joshua Holbrook <josh.holbrook@gmail.com>
2012-06-27 23:47:10 -0700

    [minor] Noticed I was basenaming against .js extensions when the directories from readdir were fine

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var fs = require('fs'),
-    path = require('path');
+var fs = require('fs');
 
-fs.readdirSync(path.resolve(__dirname, 'node_modules')).forEach(function (p) {
-  var module = path.basename(p, '.js');
-
+fs.readdirSync(path.resolve(__dirname, 'node_modules')).forEach(function (module) {
+  // Lazily require, attach to global (no property accessing either!)
   global.__defineGetter__(module, function () {
     return require(module);
   });
