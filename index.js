830b66c4beb49657ee310293507c4047e5c34f79
Joshua Holbrook <josh.holbrook@gmail.com>
2012-10-15 14:07:22 -0700

    [api] also expose core modules

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 var fs = require('fs');
 
-fs.readdirSync(path.resolve(__dirname, 'node_modules')).forEach(function (module) {
+// core modules
+[
+  'assert',
+  'child_process',
+  'cluster',
+  'crypto',
+  'dgram',
+  'dns',
+  'domain',
+  'events',
+  'fs',
+  'http',
+  'https',
+  'net',
+  'path',
+  'readline',
+  'repl',
+  'stream',
+  'tls',
+  'tty',
+  'url',
+  'util',
+  'vm',
+  'zlib'
+].forEach(expose);
+
+// npm modules
+fs.readdirSync(path.resolve(__dirname, 'node_modules')).forEach(expose);
+
+function expose(module) {
   // Lazily require, attach to global (no property accessing either!)
   global.__defineGetter__(module, function () {
     return require(module);
   });
-});
+}
 
 module.exports = undefined;
