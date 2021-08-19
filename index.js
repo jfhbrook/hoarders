78dc1271030a9c9768ff846eeef8a514c2bdb158
Josh Holbrook <josh.holbrook@gmail.com>
2021-08-19 16:50:04 -0400

    Holy shit it works! T__T o7 AMERICAAA

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var assert = require('assert');
 var fs = require('fs');
+var spawnSync = require('child_process').spawnSync;
+
+var log = process.logging ? process.logging : function () {};
+
+//
+// If you want to opt a package out of hoarders, add it to this list and
+// submit a pull request.
+//
+exports.haters = require('./haters.json');
 
 // core modules
-[
+exports.coreModules = [
   'assert',
   'child_process',
   'cluster',
@@ -24,16 +34,50 @@ var fs = require('fs');
   'util',
   'vm',
   'zlib'
-].forEach(expose);
+].reduce(function (acc, coreModule) {
+  acc[coreModule] = true;
+  return acc;
+}, {});
 
-// npm modules
-fs.readdirSync(path.resolve(__dirname, 'node_modules')).forEach(expose);
+module.exports = new Proxy(exports, {
+  get: function (target, prop, receiver) {
+    return getModule(prop);
+  }
+});
 
-function expose(module) {
-  // Lazily require, attach to global (no property accessing either!)
-  global.__defineGetter__(module, function () {
-    return require(module);
-  });
+function getModule(moduleName) {
+  if (moduleName === 'hoarders') {
+    return module.exports;
+  }
+  var module = null;
+  try {
+    module = require(moduleName);
+  } catch (err) {
+    assert(
+      !exports.coreModules[moduleName],
+      'a core module should require first try'
+    );
+    if (exports.haters[moduleName]) {
+      throw new Error(
+        'refusing to install ' +
+          moduleName +
+          ' because ' +
+          exports.haters[moduleName].author.toUpperCase() +
+          ' is a HATER 😤!'
+      );
+    }
+    log('error on first require(%module): %err', {
+      err: err,
+      module: moduleName
+    });
+    log('attempting to npm install %module...', {module: moduleName});
+    var npmResult = spawnSync('npm', ['install', moduleName], {
+      stdio: ['pipe', 'inherit', 'inherit']
+    });
+    if (npmResult.error) throw npmResult.error;
+    if (npmResult.status)
+      throw new Error('npm install returned status: ' + npmResult.status);
+    module = require(moduleName);
+  }
+  return module;
 }
-
-module.exports = undefined;
